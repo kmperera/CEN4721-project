d314049434bff53885a9681f1bb8ab04083f3ba5
Katie <katieperera01@gmail.com>
2023-10-24 18:05:08 -0400

    added more data to the array

--- Design A/script.js
+++ Design A/script.js
@@ -10,25 +10,43 @@ const message2 = 'Gator Course Searcher'
 
 document.querySelector('#header2').innerHTML = message2
 
-//fake data for testing purposes
+//need to add more data
 var array = [
     {
         className: 'Programming Fundamentals 1',
-        classCode: 'COP 3502',
-        prereqs: 'None',
-        professor: 'Dr. Sean Szumlanski',
-        time: 'MWF 1:30-2:20',
-        classroom: 'HPA 114',
-        links: 'Syllabus: https://www.cs.ucf.edu/~szum/courses/cop3502f19/syllabus.html, Textbook: https://www.amazon.com/Java-Software-Solutions-Foundations-Program/dp/0134462025/ref=sr_1_1?keywords=java+software+solutions&qid=1567626283&s=gateway&sr=8-1'
+        classCode: 'COP3502',
+        prereqs: 'MAC2311',
+        professor: 'Amanpreet Kapoor and Sanethia Thomas',
+        time: 'TR 10:40 AM - 11:30 AM, W 12:50 PM - 2:45 PM',
+        classroom: 'CAR 0100, CSE E309',
+        links: 'Syllabus: link, Textbook: link'
     },
     {
-        className: 'Programming Fundamentals 2',
-        classCode: 'COP 3503',
-        prereqs: 'COP 3502',
-        professor: 'Dr. Sean Szumlanski',
-        time: 'MWF 3:30-4:20',
-        classroom: 'HPA 114',
-        links: 'Syllabus: https://www.cs.ucf.edu/~szum/courses/cop3503f19/syllabus.html, Textbook: https://www.amazon.com/Java-Software-Solutions-Foundations-Program/dp/0134462025/ref=sr_1_1?keywords=java+software+solutions&qid=1567626283&s=gateway&sr=8-1'
+        className: 'Programming Fundamentals 1',
+        classCode: 'COP3502',
+        prereqs: 'MAC2311',
+        professor: 'Daniel Delgado',
+        time: 'TR 10:40 AM - 11:30 AM, T 5:10 PM - 7:05 PM',
+        classroom: 'ROG 0129, CSE E312',
+        links: 'Syllabus: link, Textbook: link'
+    },
+    {
+        className: 'Introduction to Literature',
+        classCode: 'LIT2000',
+        prereqs: 'ENC1101',
+        professor: 'Benjamin Hebblethwaite',
+        time: 'Online',
+        classroom: 'Online',
+        links: 'Syllabus: link, Textbook: link'
+    },
+    {
+        className: 'Introduction to Literature',
+        classCode: 'LIT2000',
+        prereqs: 'ENC1101',
+        professor: 'Galina Rylkova',
+        time: 'Online',
+        classroom: 'Online',
+        links: 'Syllabus: link, Textbook: link'
     }
 ]
 
@@ -57,11 +75,13 @@ var newArray = [];
 document.getElementById("search").addEventListener("keyup", function(){
     var search = this.value.toLowerCase(); // Corrected this line
     
+    //doesn't include time and links to be searchable
     newArray = array.filter(function(val){
         return val.className.toLowerCase().includes(search) || 
                val.classCode.toLowerCase().includes(search) || 
                val.professor.toLowerCase().includes(search) || 
-               val.classroom.toLowerCase().includes(search);
+               val.classroom.toLowerCase().includes(search) ||
+               val.prereqs.toLowerCase().includes(search);
     });
 
     showtable(newArray);
